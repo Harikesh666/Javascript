340eba83e68f1aa4809a4d5530845651bfaf39ac
Harikesh666 <88932405+Harikesh666@users.noreply.github.com>
2024-10-19 15:03:08 +0000

    new commit

---
+++ Js_working.js
@@ -0,0 +1,14 @@
+/* 
+"Everything in JavaScript happens inside the execution context", you can assume this execution context to be a big box or a container in which whole JS code is executed.
+
+The execution context consists of two parts(components) the Variable environment(memory component), it is a sort of environment in which all the variables and functions are stored in key:value pairs. 
+
+The second component of the execution context is the Thread of Execution (code component), this is the place where code is executed one line at a time.
+
+"JavaScript is a synchronous, single-threaded language", this means that JS can only execute one command at a time and in a specific order, it can only go to the next line once the current line has been executed.
+
+Javascript is not possible without this beautiful execution context
+
+I know you must have heard about AJAX where 'A' stands for asynchronous, but we will come to that later
+
+*/
